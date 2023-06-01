b75ebf2068311d8dd961c5ce38f5f1093fe668f5
Leo <112168257+leloa001@users.noreply.github.com>
2023-06-01 05:19:47 +0200

    added list of ingredients saved in DB

--- pages/FAQ.tsx
+++ pages/FAQ.tsx
@@ -1,7 +1,8 @@
 import FaqComponent from "@/components/FaqComponent"
 
 export default function FAQ () {
-    const faq = [{header: 'Hva er formålet med nettsiden?', content: 'Lurer du ofte på hva du skal ha til middag, eller kanskje du pleier å sitte igjen med mye rester du ikke får brukt? Da er denne siden for deg! Mitt ønske med denne "appen" er å gjøre det enklere å planlegge middager, minske enkeltpersoners matsvinn og lage ett dedikert sted hvor man kan lagre og utforske oppskrifter'}, {header: "header", content: "dette er innhold"}]
+    const faq = [{header: 'Hva er formålet med nettsiden?', content: 'Lurer du ofte på hva du skal ha til middag, eller kanskje du pleier å sitte igjen med mye rester du ikke får brukt? Da er denne siden for deg! Mitt ønske med denne "appen" er å gjøre det enklere å planlegge middager, minske enkeltpersoners matsvinn og lage ett dedikert sted hvor man kan lagre og utforske oppskrifter'}, 
+                 {header: "header", content: "dette er innhold"}]
 
 return(
     <div className=" h-full w-full mt-12 p-4 justify-evenly items-center flex flex-wrap space-x-4 space-y-4" >
